d56f6d3e2530801da42495b226197f0205994d53
Ryan Routsong <rroutson@utk.edu>
2019-03-24 21:14:09 -0400

    change beginswith to startswith

--- lib/GEMMA_GWAS/Util/GWASReportUtils.py
+++ lib/GEMMA_GWAS/Util/GWASReportUtils.py
@@ -132,7 +132,7 @@ class GWASReportUtils:
                                         raise KeyError(e)
 
                         if snp[1] is '.':
-                            if str(snp[0]).beginswith('C'):
+                            if str(snp[0]).startswith('C'):
                                 snp[1] = str(snp[0]) + '_' + str(snp[2])
                             else:
                                 snp[1] = 'Chr' + str(snp[0]) + '_' + str(snp[2])
