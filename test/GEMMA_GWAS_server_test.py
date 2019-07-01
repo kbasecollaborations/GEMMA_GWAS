cc998e2b3d58d969f8498965a86875990892999f
Ryan Routsong <rroutson@utk.edu>
2019-07-01 10:29:30 -0400

    changes to test dataset

--- test/GEMMA_GWAS_server_test.py
+++ test/GEMMA_GWAS_server_test.py
@@ -128,7 +128,7 @@ class GEMMA_GWASTest(unittest.TestCase):
             #'variation': '29104/23/1',
             #'trait_matrix': '29104/12/1',
             #'model': 0
-            
+
             'workspace_name': 'rmr:narrative_1559592440406',
             'variation': '29104/23/1',
             'trait_matrix': '29104/12/1',
